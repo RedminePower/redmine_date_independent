e31d8d9e5604fa1bd040864c773f1643fa2b84ed
RedminePower <redmine.power.com@gmail.com>
2023-03-23 21:07:15 +0900

    まずは、init.rbを追加

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_auto_close do
+  name 'Redmine Parrent Date Independent plugin'
+  author 'Redmine Power'
+  description 'This plugin allows the start and due dates of parent issue to be set independently of child issues.'
+  version '1.0.0'
+  url 'https://github.com/RedminePower/redmine_parrent_date_independent'
+  author_url 'https://www.en.redmine-power.com/'
+end
