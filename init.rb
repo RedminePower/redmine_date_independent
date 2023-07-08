8ee56a311bf4f98e704f475c52b172428143944a
RedminePower <redmine.power.com@gmail.com>
2023-07-08 15:01:06 +0900

    Update version number to 1.0.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_date_independent do
   name 'Redmine Date Independent plugin'
   author 'Redmine Power'
   description 'This plugin allows the start and due dates of parent issue to be set independently of child issues.'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/RedminePower/redmine_date_independent'
   author_url 'https://www.en.redmine-power.com/'
 
